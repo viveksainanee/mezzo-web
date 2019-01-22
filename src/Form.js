e9e2aedbcef3d51ffb418dfc2d2b0d1020c1cfeb
Vivek Sainanee <viveksainanee@gmail.com>
2019-01-22 15:30:04 -0800

    initial form

---
+++ src/Form.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loc1: '',
+      loc2: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="loc1"
+          onChange={this.handleChange}
+          value={this.state.loc1}
+          placeholder="First Location"
+        />
+
+        <input
+          name="loc2"
+          onChange={this.handleChange}
+          value={this.state.loc2}
+          placeholder="Second Location"
+        />
+      </form>
+    );
+  }
+}
+
+export default Form;
---
+++ src/MezzoHome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Form from './Form';
+
+class MezzoHome extends Component {
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    );
+  }
+}
+
+export default MezzoHome;
